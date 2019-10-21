3288908b2175cd804a7a12c0ee6be338128e90ae
Carson Clarke-Magrab <carsonclarke570@gmail.com>
2019-10-21 19:17:59 -0400

    Updates

---
+++ precache-manifest.9dac390f5308a8de312e060927177d02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec42e095aeec4c1f30aee5a47ee5cce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7278f467e90d34609f3",
+    "url": "/static/js/2.164b6632.chunk.js"
+  },
+  {
+    "revision": "9c6acfefe306aff3679b",
+    "url": "/static/js/main.b5521b26.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  }
+]);
