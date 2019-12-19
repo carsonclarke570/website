f4440b206e093825315f30e0703ae485cbe2a1e8
Carson Clarke-Magrab <carsonclarke570@gmail.com>
2019-12-19 13:00:57 -0500

    Updates

---
+++ precache-manifest.dafc17b5d0a5efe4f3ffed0711b8e4b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b686735aed0bb3fb3b8990fdc15b7678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc195663ca9e60eb5373",
+    "url": "/static/js/2.bbad2710.chunk.js"
+  },
+  {
+    "revision": "284813d278e59aec9755",
+    "url": "/static/js/main.7c4e5477.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  },
+  {
+    "revision": "a947b3fe92d0589e3ad45b2870d9372b",
+    "url": "/static/media/resume.a947b3fe.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dac390f5308a8de312e060927177d02.js"
+  "/precache-manifest.dafc17b5d0a5efe4f3ffed0711b8e4b2.js"
 );
 
 self.addEventListener('message', (event) => {
